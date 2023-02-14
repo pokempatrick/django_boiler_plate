bf7a91b1b2187b637d7d139d3c7f4ea6cf619ccf
Pokem <pokempatrick@yahoo.fr>
2023-02-14 03:21:30 +0100

    documentation plugin added

--- my_first_app/settings.py
+++ my_first_app/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'drf_yasg',
     'rest_framework',
     'todos',
     'authentification',
