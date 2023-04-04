dec25dfd4705253b9d8820a132d537a2b9d912e5
Pokem <pokempatrick@yahoo.fr>
2023-04-04 09:07:49 +0100

    serialise nested object validation

--- reservations/serializer.py
+++ reservations/serializer.py
@@ -4,7 +4,16 @@ from reservations.models import Reservations
 from reservations.models import Validations
 
 
-class ReservationSerializer(serializers.ModelSerializer):
+class ValidationPureSerializer(serializers.ModelSerializer):
+    user = RegisterSerilizer(
+        read_only=True, default=None)
+
+    class Meta:
+        model = Validations
+        fields = '__all__'
+
+
+class ReservationPureSerializer(serializers.ModelSerializer):
     customer = RegisterSerilizer(
         read_only=True, default=None)
 
@@ -16,11 +25,26 @@ class ReservationSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class ReservationSerializer(serializers.ModelSerializer):
+    customer = RegisterSerilizer(
+        read_only=True, default=None)
+
+    owner = RegisterSerilizer(
+        read_only=True, default=None)
+
+    validations = ValidationPureSerializer(
+        read_only=True, default=None, many=True)
+
+    class Meta:
+        model = Reservations
+        fields = '__all__'
+
+
 class ValidationSerializer(serializers.ModelSerializer):
     user = RegisterSerilizer(
         read_only=True, default=None)
 
-    reservation = ReservationSerializer(
+    reservation = ReservationPureSerializer(
         read_only=True, default=None)
 
     class Meta:
